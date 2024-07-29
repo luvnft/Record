a76230064d186e7fba1a772e86dd175572985416
The Wizard of Hahz <info@luvnft.com>
2024-07-29 07:21:44 -0400

    updated

--- os-records/src/Home.tsx
+++ os-records/src/Home.tsx
@@ -3,18 +3,19 @@ function Home() {
         <div>
             <h1>Welcome to ATL5D Mixtape on the SUI Blockchain</h1>
             <h2>Artists!</h2>
-            <p>Earn 💯%</p>
-            <h2>Worldwide Distribution!</h2>
-            <p>Sell your music to the world without a middleman.</p>
-            <h2>Subscribe to ATL5D Records today!</h2>
+            <p>ATL5D ended modern day artist slavery is ended in 2024.</p>
+            <h2>Fans</h2>
+            <p>Re-sell your music to the world without a middleman.</p>
+            <h2>Subscribe to <a href="https://discord.com/servers/atl5d-1244450286337003520" target="_blank" rel="noopener noreferrer">ATL5D Discord</a> as Midtown/Gold Room subscriber to create a branded channel for your fans.</h2>
             <div>
                 <ul>
-                    <li>100% Ownership of your music and copyrights</li>
-                    <li>Social media AR Effect promo</li>
-                    <li>Transparent and fair royalties</li>
-                    <li>Global distribution through the SUI blockchain</li>
-                    <li>Community support in the ATL5D Discord #music channel</li>
-                    <li>Create your own digital merch store</li>
+                    <li>💯 Ownership of your music and copyrights</li>
+                    <li>📣 Social media AR Effect promo</li>
+                    <li>💿 Transparent and fair royalties</li>
+                    <li>🪩 Challenge fans to create a dance</li>
+                    <li>📺 Tribe support in the ATL5D Discord #music channel</li>
+                    <li>🏪 Create a digital merch store</li>
+                    <li>🎤 Host live shows in ATLverse</li>
                 </ul>
             </div>
             <p>All track copyrights owned by block owner.</p>
