577e271d1d6075a781255f2f30a1d45198a7b49f
The Wizard of Hahz <info@luvnft.com>
2024-07-29 08:01:40 -0400

    updated

--- os-records/src/App.tsx
+++ os-records/src/App.tsx
@@ -2,7 +2,6 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import './index.css'
 import NavBar from './components/NavBar.tsx'
-import Home from './Home.tsx'
 import Market from './Market.tsx'
 import MyTracks from './MyTracks.tsx'
 
@@ -12,7 +11,6 @@ function App() {
     <div>
       <NavBar />
       <Routes>
-          <Route path="/" element={<Home />} />
           <Route path="/market" element={<Market />} />
           <Route path="/mytracks" element={<MyTracks />} />
       </Routes>
