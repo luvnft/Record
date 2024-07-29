07624e7bf5b485d7379d7919627d1d87ed795233
The Wizard of Hahz <info@luvnft.com>
2024-07-29 07:30:37 -0400

    updated

--- os-records/src/Home.tsx
+++ os-records/src/Home.tsx
@@ -6,7 +6,7 @@ function Home() {
             <p>ATL5D ended modern day artist slavery is ended in 2024.</p>
             <h2>Fans</h2>
             <p>Re-sell your music to the world without a middleman.</p>
-            <h2>Subscribe to <a href="https://discord.com/servers/atl5d-1244450286337003520" target="_blank" rel="noopener noreferrer">ATL5D Discord</a> as Midtown/Gold Room subscriber to create a branded channel for your fans.</h2>
+            <h2>Subscribe to <a href="https://discord.com/servers/atl5d-1244450286337003520" target="_blank" rel="noopener noreferrer">ATL5D Discord</a> to create a branded channel for your fans.</h2>
             <div>
                 <ul>
                     <li>💯 Ownership of your music and copyrights</li>
